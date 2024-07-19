d04867257644aa64c75fde72ed9728fe51ddc0ab
Enes Tunahan <enesstunahan@gmail.com>
2024-07-19 17:39:51 +0300

    api yazıldı

---
+++ API/API/Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDbContext _context;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id , Employee employee)
+        {
+            if(id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }              
+            }
+
+            return NoContent();
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+           _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new {id = employee.Id} , employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return employee;
+        }
+
+        private bool EmployeeExist(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+
+
+
+    }
+}
